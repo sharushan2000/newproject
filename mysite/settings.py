88f41ada27914d81898b130a46d943b0177d05f5
sharushan2000 <sharushan0000@gmail.com>
2023-06-06 12:34:04 -0400

    basic setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pdf',
 ]
 
 MIDDLEWARE = [
@@ -50,11 +52,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+TEMP_DIR = os.path.join(BASE_DIR , 'templates')
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMP_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
